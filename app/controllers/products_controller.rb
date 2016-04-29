ad6418d0da05da4a7cb94b4800dfe41146bc6226
hisayohorie <hisayohorie@gmail.com>
2016-04-29 15:11:37 -0400

    added search form

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,47 @@
+class ProductsController < ApplicationController
+  # def index
+  #   @products = Product.all
+  # end
+  def index
+    @products = if params[:search]
+      Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+    else
+      Product.all
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+
+    if current_user
+      @review = @product.review.build
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+  def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to products_path
+  end
+  private
+   def product_params
+     params.require(:product).permit(:name, :description, :price_in_cents)
+   end
+end
